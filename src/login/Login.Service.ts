e6e4093a1dbb7a3a80d4bccc5f78044e902bcdb0
ThomDeV-bit <120723852+ThomDeV-bit@users.noreply.github.com>
2023-07-14 18:15:21 -0300

    feat Login

---
+++ src/login/Login.Service.ts
@@ -0,0 +1,46 @@
+import express, { Express, Router, Request, Response } from "express";
+import { CheckValidator, UserValidator } from "../checkValidator/Checkvalidator";
+import { v4 } from "uuid";
+import bcrypt from 'bcrypt'
+import { UserModel } from "../user/User.Model";
+import Mensage from "../status/Mensage";
+
+
+
+const loginService = Router()
+
+loginService.use(express.json())
+
+const validator = new CheckValidator()
+const userValidator = new UserValidator()
+
+loginService.post('/singIn', validator.checkValidator, async (req: Request, resp: Response) => {
+    const body: UserModel = req.body
+    const user: UserModel | null = await UserModel.findOne({
+        where: {
+            mail: body.mail
+        }
+    })
+    if (user) {
+        try {
+            const passwordHasd: string | undefined = user?.dataValues.password
+            const hashed = await bcrypt.compare(body.password!, passwordHasd!)
+            if (hashed == true) {
+                return resp.status(200).json(new Mensage('User Connect',user, true))
+            }
+            else {
+                return resp.status(203).json(new Mensage('Incorrect Credentials',user,false))
+            }
+
+        } catch (error) {
+            return resp.status(500).json(new Mensage('Internal Error', error, false))
+        }
+
+    }
+    else{
+        resp.status(404).json(new Mensage('Unavalible Account ', req.body.mail,false))
+    }
+})
+
+
+    export default loginService
