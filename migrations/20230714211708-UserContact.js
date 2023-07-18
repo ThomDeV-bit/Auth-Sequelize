07760a18eb1701fa963fba06e9d8064fc317f39d
ThomDeV-bit <120723852+ThomDeV-bit@users.noreply.github.com>
2023-07-18 10:50:10 -0300

    feat UserContact Migration

---
+++ migrations/20230714211708-UserContact.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return await queryInterface.createTable("Contacts",
+      {
+
+        id_contact: {
+          primaryKey: true,
+          type: DataTypes.UUID,
+          allowNull: false
+        },
+
+
+        userID: {
+          type: DataTypes.UUID,
+          allowNull: false,
+          references: { model: 'Users', key: 'id' }
+
+        },
+
+        fullName: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        adress: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+
+        number: {
+          type: DataTypes.STRING,
+          allowNull: false,
+
+        },
+
+
+        createdAt: {
+          allowNull: false,
+          type: DataTypes.DATE
+        },
+
+        updatedAt: {
+
+          allowNull: false,
+          type: DataTypes.DATE
+        }
+      }
+    
+    )
+  },
+
+  async down(queryInterface, Sequelize) {
+    return await queryInterface.dropTable("Contacts")
+  }
+};
+  
+
