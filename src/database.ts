31f06117942ec6c9b9d94394f471b29e8183a5f2
ThomDeV-bit <120723852+ThomDeV-bit@users.noreply.github.com>
2023-07-18 10:52:45 -0300

    fix database config

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('mysql://root:root@localhost:3307/smart',{
+const db = new Sequelize('mysql://root:root@localhost:3310/smart',{
     storage : './database.ts',
     dialect : 'mysql',
     define : {
